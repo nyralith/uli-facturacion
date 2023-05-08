ceb167d44554b8bc793403c01f98b0f18f5706cc
nicolas.delgado <nicolas.delgado@birchmangroup.com>
2023-05-08 13:32:55 -0300

    se crea componente facturas y se agrega formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FacturasComponent } from './components/facturas/facturas.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FacturasComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule, 
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+
+
+}
---
+++ src/app/components/facturas/facturas.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-facturas',
+  templateUrl: './facturas.component.html',
+  styleUrls: ['./facturas.component.scss']
+})
+export class FacturasComponent {
+
+  editOrder: any;
+  orderData: any = []
+
+  afiliadoForm = new FormGroup({
+    numAfiliado: new FormControl('', Validators.required),
+    nombreAfiliado: new FormControl('', Validators.required)
+  })
+
+  analisisForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.analisisForm = this.fb.group({
+      codigo: ['', Validators.required],
+      analisis: ['', Validators.required],
+      importe: ['', Validators.required],
+    })
+
+
+  }
+
+  addOrder() {
+    console.log(this.analisisForm.controls['codigo'].value)
+  }
+
+  addNewAnalisis() {
+    this.orderData.push('hola chiketitapepena')
+  }
+
+  deleteAnalisis() {
+
+  }
+
+}
