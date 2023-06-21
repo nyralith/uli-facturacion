1be06ed956a7875b7b42540e583f1c77d1d2afba
HeavyGunner17 <heavygunner17alv@gmail.com>
2023-06-21 18:49:00 -0300

    se creo la 3ra pestaña con datos

--- src/app/components/resumen-facturacion-paciente/resumen-facturacion-paciente.component.ts
+++ src/app/components/resumen-facturacion-paciente/resumen-facturacion-paciente.component.ts
@@ -1,7 +1,9 @@
+import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import * as html2pdf from 'html2pdf.js'
-
-
+import { Service } from '../service/data.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-resumen-facturacion-paciente',
@@ -9,8 +11,50 @@ import * as html2pdf from 'html2pdf.js'
   styleUrls: ['./resumen-facturacion-paciente.component.scss']
 })
 export class ResumenFacturacionPacienteComponent {
-
+  allData: any;
+  service: any;
+  filteredData: any;
+  ELEMENT_DATA: any;
+  dateForm: any;
+  dataSource = new MatTableDataSource<any>
   
+  constructor(private services: Service){
+
+    }
+  async ngOnInit() {
+    this.getAllData()
+  }
+
+  filterDate(date: any) {
+    this.allData
+    this.filteredData
+
+    this.dateForm = new FormGroup({
+      fecha: new FormControl()
+    })
+
+      
+  
+
+    const fechaFactura: any = date;
+    return formatDate(fechaFactura, 'MM/yyyy', 'en-US')
+  }
+
+  async getAllData() {
+    this.allData = await this.service.getAllData();
+    console.log(this.allData[0])
+  }
+
+  async getFilteredData() {
+    this.filteredData = await this.service.getFilteredData(this.filterDate(this.dateForm.controls['fecha'].value).toString());
+
+    console.log(this.filterDate(this.dateForm.controls['fecha'].value).toString())
+    console.log(this.filteredData);
+
+    this.ELEMENT_DATA = this.filteredData
+  }
+
+
   downloadPdf() {
     let element = document.getElementById('table');
     let opt = {
