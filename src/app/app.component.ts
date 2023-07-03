34c405b0320b4d76a10b85b2f2bd6df0b5eb9816
HeavyGunner17 <heavygunner17alv@gmail.com>
2023-07-03 19:51:27 -0300

    se instalo nueva libreria de pdf y se agrego a resumen como prueba el codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import * as html2pdf from 'html2pdf.js'
-
+import jsPDF from 'jspdf';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import htmlToPdfmake from 'html-to-pdfmake';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +13,21 @@ import * as html2pdf from 'html2pdf.js'
 })
 export class AppComponent {
   title = 'uli-facturas';
+
+
+  @ViewChild('pdfTable') pdfTable: ElementRef;
+  
+  public downloadAsPDF() {
+    const doc = new jsPDF();
+   
+    const pdfTable = this.pdfTable.nativeElement;
+   
+    let html = htmlToPdfmake(pdfTable.innerHTML);
+     
+    const documentDefinition = { content: html };
+    pdfMake.createPdf(documentDefinition).open(); 
+     
+  }
 //   downloadPdf(){
 //     var element = document.getElementById('table');
 // var opt = {
